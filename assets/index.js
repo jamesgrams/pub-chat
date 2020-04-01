d3f9ba746b441d2c160f78b358979a89870f7752
James Grams <jrg@pavilion.local>
2020-04-01 13:29:56 -0400

    Lower framerate

--- assets/index.js
+++ assets/index.js
@@ -305,7 +305,7 @@ function connectToSignalServer() {
             video: {
                 width: { min: 320, ideal: 320 },
                 height: { min: 240 },
-                frameRate: 30,
+                frameRate: 10,
                 facingMode: "user"
             },
             audio: {
